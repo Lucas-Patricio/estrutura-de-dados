1428b30cbbc6a2aa7685061cea8cd5455f9cf5dd
Lucas Patrício <139584567+Lucas-Patricio@users.noreply.github.com>
2024-05-20 14:52:30 -0300

    ME Tabela Hash adicionada

---
+++ estrutura-de-dados/src/HashTable/Teste910.java
@@ -0,0 +1,24 @@
+package HashTable;
+import java.util.List;
+
+public class Teste910 {
+    public static void main(String[] args) {
+        Exercicio910 conjunto = new Exercicio910();
+        conjunto.adiciona("Palavra1");
+        conjunto.adiciona("Palavra2");
+        conjunto.adiciona("Palavra3");
+
+        List<String> palavras = conjunto.pegaTodas();
+        System.out.println("antes de remover");
+        for (String palavra : palavras) {
+            System.out.println(palavra);
+        }
+        conjunto.remove("Palavra1");
+        palavras = conjunto.pegaTodas();
+        System.out.println("\ndepois de remover");
+        for (String palavra : palavras) {
+            System.out.println(palavra);
+        }
+
+    }
+}
